ab83e8b3169fe100b166007d04b78a9d0cd0d107
Olep8h <panov@student.vspj.cz>
2022-11-20 23:39:44 +0200

    logic inner progres

--- src/index.js
+++ src/index.js
@@ -21,3 +21,50 @@ function Result({ correct }) {
         </div>
     );
 }
+
+function Game({ step, question, onClickVariant }) {
+    const percentage = Math.round((step / questions.length * 100));
+
+    return (
+        <>
+            <div className="progress">
+                <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
+            </div>
+            <h1>{question.title}</h1>
+            <ul>
+                {question.variants.map((text, index) => (
+                    <li onClick={() => onClickVariant(index)} key={text}>{text}</li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+function App() {
+    const [step, setStep] = React.useState(0);
+    const [correct, setCorrect] = React.useState(0);
+    const question =  questions[step];
+
+    const onClickVariant = (index) => {
+        console.log(step, index);
+        setStep(step + 1);
+
+        if (index === question.correct) {
+            setCorrect(correct + 1);
+        }
+    };
+
+
+    return (
+        <div className="App">
+            {
+                step !== questions.length ? (
+                    <Game step={step} question={question} onClickVariant={onClickVariant} />
+                ) : (
+                    <Result correct={correct} />
+                )}
+        </div>
+    );
+}
+
+export default App;
